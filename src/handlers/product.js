32262e2f22c28f6dfbd6c956aa8a81c507fcfd97
Alex <alex.talha@original.io>
2019-07-05 18:05:56 -0300

    subindo os arquivos

---
+++ src/handlers/product.js
@@ -0,0 +1,12 @@
+const getAll = (request, h) => {
+    return 'Hello Programador'
+}
+
+const save = () => {
+    return 'Oi'
+}
+
+module.exports = {
+    getAll,
+    save
+};
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+const productHandler = require('./handlers/product')
+
+module.exports = [{
+        method: 'GET',
+        path: '/api/v1/products',
+        handler: productHandler.getAll
+    },
+    {
+        method: 'POST',
+        path: '/api/v1/products',
+        handler: productHandler.save
+    }
+]
